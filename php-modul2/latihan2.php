3d393751dae8be69bcc2f4bef1a0b2e45e13a9a7
dhani1711 <46438931+dhani1711@users.noreply.github.com>
2019-01-07 20:29:40 +0700

    Add files via upload

---
+++ php-modul2/latihan2.php
@@ -0,0 +1,11 @@
+<?php
+    $x='4';
+    $y='5';
+    
+    echo "<h3> Hasil Operasi Menggunakan Ooperaator Perbandingan</h3>";
+    echo "x = $x <br>";
+    echo "y = $y <br>";
+    if ($x == $y){
+        echo "x sama dengan y";
+    }
+?>
